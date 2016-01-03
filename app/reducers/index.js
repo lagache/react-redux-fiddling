7e450703905ad97db3129b9fb245dafd8b23b568
christophe gachiniard <christophe.gachiniard@gmail.com>
2016-01-03 18:58:36 +1300

    Create a new reducer for tiles (empty) in order to remove a warning in the console. To be continued

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerStateReducer } from 'redux-router';
 import cars from './cars';
+import tiles from './tiles';
 
 const rootReducer = combineReducers({
   cars,
+  tiles,
   router: routerStateReducer
 });
 
---
+++ app/reducers/tiles.js
@@ -0,0 +1,12 @@
+//import { ADD_CAR } from '../actions';
+
+// TODO no action for the moment
+
+export default function tiles(state = [], action) {
+  switch (action.type) {
+  //case ADD_CAR:
+  //  return [...state, action.payload];
+  default:
+    return state;
+  }
+}
