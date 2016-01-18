ebfa5968de09f32f897de259863db4dceeb84ac8
Christophe Gachiniard <christophe_gachiniard@westpac.co.nz>
2016-01-18 13:55:30 +1300

    change game over page and reduce countdown from 3 to 2 secs between 2 levels

--- app/containers/Header/header.js
+++ app/containers/Header/header.js
@@ -22,30 +22,44 @@ class Header extends Component {
     }
 
     render() {
-        // Injected by connect() call:
-        const { dispatch } = this.props;
-
         if (this.props.tiles.gameOver) {
+            let textScore = "you need to train a bit more";
+            let nbTileFound = this.props.tiles.nbTileFound;
+            // Injected by connect() call:
+            const { dispatch } = this.props;
+
+            if (nbTileFound > 10 && nbTileFound <= 20) {
+                textScore = "not too bad but you can do better";
+            } else if (nbTileFound > 20 && nbTileFound <= 30) {
+                textScore = "that's really good!"
+            } else if (nbTileFound > 30) {
+                textScore = "you are a champion!"
+            }
             return (
                 <div className="header">
-                    <br/>
-                    <Link to="/">Menu</Link>
+                    <Link to="/" className="btn btn-primary"> menu </Link>
                     <br/><br/>
-                    <button onClick={() => this.startTheGame()} >
+                    <button onClick={() => this.startTheGame()} className="btn btn-success">
                         START NEW GAME
                     </button>
 
-                    <br/>
-                    <p> GAME OVER... {this.props.tiles.nbTileFound} in a row, not too bad, bro. </p>
+                    <div className="result-area">
+                        <h4>GAME OVER...</h4>
+                        <p className="result-text">{this.props.tiles.nbTileFound} in a row, {textScore}</p>
+                    </div>
                 </div>
                 );
         } else if (this.props.tiles.countdown){
+            let duration = 3;
+            if(this.props.tiles.level > 0) {
+                duration = 2;
+            }
             return (
                 <div className="header">
-                    <br/> <br/>
-                    <h1><Countdown duration="3" /></h1>
                     <br/>
-                    <br/>
+                    <h3>Level: {this.props.tiles.level}</h3>
+                    <h3>Tiles remaining: {this.props.tiles.tilesRemaining}</h3>
+                    <h1><Countdown duration={duration} /></h1>
                 </div>
                 );
         } else {
@@ -54,6 +68,7 @@ class Header extends Component {
                     <br/>
                     <h3>Level: {this.props.tiles.level}</h3>
                     <h3>Tiles remaining: {this.props.tiles.tilesRemaining}</h3>
+                    <br/>
                 </div>
             )
         }
