be375dfb776bd1446364330bb2fcff3e666f9f8e
Christophe Gachiniard <christophe_gachiniard@westpac.co.nz>
2016-01-18 13:53:37 +1300

    change about page

--- app/containers/About/About.js
+++ app/containers/About/About.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
+import './about.scss';
 
 export default class About extends Component {
 
     render() {
-        return (<p> it is all about learning how to use react. Christophe (lagache)</p>);
+        return (
+            <div className ="about">
+                <p className="about-text">It's all about learning how to use react.</p>
+                <p className="about-text">Developers - Christophe / Xavier</p>
+                <p className="about-text">Testers - Anais</p>
+            </div>
+        );
     }
 }
