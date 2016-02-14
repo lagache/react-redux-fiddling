0aca01ef655584ebb341655b1717425f25509f2a
christophe gachiniard <christophe.gachiniard@gmail.com>
2016-02-14 16:06:04 +1300

    add build task for generating bundle.js + html

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
+const path = require('path');
+
+const PATHS = {
+  app: path.join(__dirname, 'app/index.js'),
+  build: path.join(__dirname, 'build')
+};
+
 module.exports = {
-    entry: './app/index.js',
-    output: {
-        filename: 'public/bundle.js'
-    },
+  // Entry accepts a path or an object of entries. We'll be using the
+  // latter form given it's convenient with more complex configurations.
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel'
+            },
+            // SASS
+            {
+              test: /\.scss$/,
+              loader: 'style!css!sass'
             }
         ]
     }
-}
+};
